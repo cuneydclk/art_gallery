bea23c70d7937555a5a53bbeae6b6fbb1b193b2f
cuneydclk <cuneydclk@gmail.com>
2025-07-25 19:18:11 +0300

    Max url lenght set 900

--- artworks/models.py
+++ artworks/models.py
@@ -11,7 +11,7 @@ from decimal import Decimal
 class Artwork(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=255, unique=True, blank=True, help_text="Unique URL-friendly identifier. Leave blank to auto-generate from title.")
-    image_placeholder_url = models.URLField(max_length=500, blank=True, null=True, help_text="URL to a placeholder image for now.")
+    image_placeholder_url = models.URLField(max_length=900, blank=True, null=True, help_text="URL to a placeholder image for now.")
     current_owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="owned_artworks")
     is_for_sale_direct = models.BooleanField(default=False)
     direct_sale_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
